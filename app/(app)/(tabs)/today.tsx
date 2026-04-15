8cc59519186f277b779657c2e4ee8c23a7ccb853
Joshua Owoyemi <tjosh.owoyemi@gmail.com>
2026-04-15 15:53:36 +0900

    fix ios build error
    Signed-off-by: Joshua Owoyemi <tjosh.owoyemi@gmail.com>

--- app/(app)/(tabs)/today.tsx
+++ app/(app)/(tabs)/today.tsx
@@ -248,6 +248,9 @@ const CompletenessSection = React.memo(function CompletenessSection({
 });
 
 const BackgroundStatusSection = React.memo(function BackgroundStatusSection({
+  backgroundTaskRegistrationCheckedAt,
+  backgroundTaskRegistrationMessage,
+  backgroundTaskRegistrationStatus,
   bufferedActivityQueueDepth,
   lastBackgroundTaskFailureAt,
   lastBackgroundTaskFailureMessage,
